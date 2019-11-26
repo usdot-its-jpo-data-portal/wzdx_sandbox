6e1a986ae54f147406be4363ee5d49a45d7cacdd
587352 <lien_julia@bah.com>
2019-11-26 18:11:12 -0500

    Add main method

--- lambda__wzdx_ingest_to_lake.py
+++ lambda__wzdx_ingest_to_lake.py
@@ -2,7 +2,6 @@
 Ingest from WZDx
 
 """
-
 from __future__ import print_function
 
 import json
@@ -20,10 +19,14 @@ FEED = os.environ['FEED']
 BUCKET = os.environ['BUCKET']
 
 
-def lambda_handler(event, context):
+def lambda_handler(event=None, context=None):
     """AWS Lambda handler. """
     # TODO: eventually have this be triggered by another lambda
     wzdx_sandbox = WorkZoneSandbox(feed=json.loads(FEED), bucket=BUCKET, logger=logger)
     wzdx_sandbox.update_from_feed()
 
     logger.info('Processed events')
+
+
+if __name__ == '__main__':
+    lambda_handler()
