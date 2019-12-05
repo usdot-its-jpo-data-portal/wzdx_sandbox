f8e0494c26fa437ac9931b91b3fea004784918cb
587352 <lien_julia@bah.com>
2019-12-05 17:46:02 -0500

    Add docstrings to s3_helper file

--- s3_helper.py
+++ s3_helper.py
@@ -1,7 +1,7 @@
-'''
+"""
 AWS and AWS S3 Helper functions.
 
-'''
+"""
 import boto3
 import botocore.exceptions
 import json
@@ -10,11 +10,24 @@ import traceback
 
 
 class AWS_helper(object):
-    '''
+    """
     Helper class for connecting to AWS.
 
-    '''
+    """
     def __init__(self, aws_profile=None, logger=False):
+        """
+        Initialization function of the AWS_helper class.
+
+        Parameters:
+            aws_profile: Optional string name of your AWS profile, as set up in
+                the credential file at ~/.aws/credentials. No need to pass in
+                this parameter if you will be using your default profile. For
+                additional information on how to set up the credential file, see
+                https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials_profiles.html
+            logger: Optional parameter. Could pass in a logger object or not pass
+                in anything. If a logger object is passed in, information will be
+                logged instead of printed. If not, information will be printed.
+        """
         self.aws_profile = aws_profile
         self.print_func = print
         if logger:
@@ -22,13 +35,13 @@ class AWS_helper(object):
         self.session = self._create_aws_session()
 
     def _create_aws_session(self):
-        '''
+        """
         Creates AWS session using aws profile name passed in or using aws
         credentials in environment variables.
 
         Returns:
             AWS session object.
-        '''
+        """
         try:
             if self.aws_profile:
                 session = boto3.session.Session(profile_name=self.aws_profile, region_name='us-east-1')
@@ -45,26 +58,29 @@ class AWS_helper(object):
 
 
 class S3Helper(AWS_helper):
-    '''
-    Helper class for connecting to AWS.
+    """
+    Helper class for connecting to and working with AWS S3.
 
-    '''
-
+    """
     def __init__(self, **kwargs):
+        """
+        Initialization function of the S3Helper class.
+
+        """
         super(S3Helper, self).__init__(**kwargs)
         self.client = self._get_client()
 
     def _get_client(self):
-        '''
+        """
         Creates S3 client.
 
         Returns:
             AWS S3 client.
-        '''
+        """
         return self.session.client('s3')
 
     def path_exists(self, bucket, path):
-        '''
+        """
         Check if S3 path exists.
 
         Parameters:
@@ -73,7 +89,7 @@ class S3Helper(AWS_helper):
 
         Returns:
             Boolean (True/False)
-        '''
+        """
         try:
             self.client.get_object(Bucket=bucket, Key=path)
             return True
@@ -81,7 +97,7 @@ class S3Helper(AWS_helper):
             return False
 
     def get_data_stream(self, bucket, key):
-        '''
+        """
         Get data stream.
 
         Parameters:
@@ -90,7 +106,7 @@ class S3Helper(AWS_helper):
 
         Returns:
             "Readable" file datastream objects
-        '''
+        """
         obj = self.client.get_object(Bucket=bucket, Key=key)
         if key[-3:] == '.gz':
             gzipped = GzipFile(None, 'rb', fileobj=obj['Body'])
@@ -100,7 +116,7 @@ class S3Helper(AWS_helper):
         return data
 
     def newline_json_rec_generator(self, data_stream):
-        '''
+        """
         Receives a data stream that is assumed to be in the newline JSON format
         (one stringified json per line), reads and returns these records as
         dictionary objects one at a time.
@@ -110,7 +126,7 @@ class S3Helper(AWS_helper):
 
         Returns:
             Iterable array of dictionary objects
-        '''
+        """
         line = data_stream.readline()
         while line:
             if type(line) == bytes:
@@ -128,7 +144,7 @@ class S3Helper(AWS_helper):
             line = data_stream.readline()
 
     def write_recs(self, recs, bucket, key):
-        '''
+        """
         Writes the array of dictionary objects as newline json text file to the
         specified S3 key in the specified S3 bucket
 
@@ -139,12 +155,12 @@ class S3Helper(AWS_helper):
 
         Returns:
             None
-        '''
+        """
         outbytes = "\n".join([json.dumps(i) for i in recs if i]).encode('utf-8')
         self.client.put_object(Bucket=bucket, Key=key, Body=outbytes)
 
     def write_bytes(self, outbytes, bucket, key):
-        '''
+        """
         Writes the bytes to the specified S3 key in the specified S3 bucket
 
         Parameters:
@@ -154,7 +170,7 @@ class S3Helper(AWS_helper):
 
         Returns:
             None
-        '''
+        """
         if type(outbytes) != bytes:
             outbytes = outbytes.encode('utf-8')
         self.client.put_object(Bucket=bucket, Key=key, Body=outbytes)
