93694c60f7c1d00a3753684ed583193641245f78
chuehlien <chuehlien@gmail.com>
2021-06-15 10:39:33 -0400

    Remove code smell

--- tests/test_wzdx_sandbox.py
+++ tests/test_wzdx_sandbox.py
@@ -5,4 +5,3 @@ import os
 class TestWZDxSandboxImports(unittest.TestCase):
     def test_imports(self):
         from wzdx_sandbox.wzdx_sandbox import ITSSandbox, WorkZoneSandbox, WorkZoneRawSandbox
-        pass
